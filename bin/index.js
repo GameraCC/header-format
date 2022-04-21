587e0f0b8104f85a3c8dbc38e8800afdd41a18b3
Gamera <gamerameshcc@gmail.com>
2022-04-20 21:43:42 -0400

    init

---
+++ bin/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const colors = require('colors')
+const { formatChromeHeaders } = require('../lib')
+
+// Get & format inputted headers because windows shell interprets double quoted header values as a different key value input argument
+let input = process.argv.slice(2, process.argv.length).join(' ').split('\n').filter(x => x != '').map(concatenated => {
+    concatenated = concatenated.split(/(: )/)
+
+    const header = {
+        key: concatenated[0],
+        value: concatenated[2]
+    }
+
+    return header
+})
+
+// Fix sec- headers with quoted versions, brands and entire header values
+const quotedBrandValueSecHeaders = ['sec-ch-ua']
+quotedBrandValueSecHeaders.forEach(key => {
+    const secHeaderIndex = input.findIndex(header => header.key === key)
+    if (secHeaderIndex > -1) {
+        input[secHeaderIndex].value = input[secHeaderIndex].value.split(', ').map(x => {
+            x = x.split(/(;v=)/)
+            return `"${x[0]}";v="${x[2]}"`
+        }).join(', ')
+    }
+})
+
+// Fix sec-ch-ua-windows header
+const quotedHeaders = ['sec-ch-ua-platform-version', 'sec-ch-ua-platform']
+quotedHeaders.forEach(key => {
+    const secHeaderIndex = input.findIndex(header => header.key = key)
+    if (secHeaderIndex > -1) input[secHeaderIndex].value = `"${input[secHeaderIndex.value]}"`
+})
+
+// Format headers
+const formatted = formatChromeHeaders(input)
+
+console.log(colors.america(formatted))
